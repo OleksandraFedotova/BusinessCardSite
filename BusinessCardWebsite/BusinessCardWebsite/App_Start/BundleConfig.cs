3d9140a51b2e2e232c611aaf1bf3b9dd784c1f04
Alexandra Fedotova <feyasasha19111996@gmail.com>
2017-02-16 20:35:42 +0200

    added bckg

--- BusinessCardWebsite/BusinessCardWebsite/App_Start/BundleConfig.cs
+++ BusinessCardWebsite/BusinessCardWebsite/App_Start/BundleConfig.cs
@@ -26,6 +26,8 @@ namespace BusinessCardWebsite
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/anim-bckg").Include(
+                      "~/Scripts/animated-bckg.js"));
         }
     }
 }
